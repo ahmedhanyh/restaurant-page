b4abfda5e7e030017ef693af3b512d8af7872312
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-02-10 16:38:06 +0200

    Added more menu items
    In index.css: decreased the bottom margin for h1 element

--- src/menuPage/menuPage.js
+++ src/menuPage/menuPage.js
@@ -1,5 +1,7 @@
-import burgerImg from "./burger.jpg";
 import "./menuPage.css";
+import burgerImg from "./burger.jpg";
+import friesImg from "./fries.jpg";
+import colaImg from "./cola.jpg";
 
 export default function loadMenuPage() {
     const menuContentDiv = document.createElement("div");
@@ -8,33 +10,39 @@ export default function loadMenuPage() {
     const menuHeading = document.createElement("h1");
     menuHeading.textContent = "Menu";
     
+    const burgerItem = createMenuItem("Burger", 5, burgerImg);
+    const friesItem = createMenuItem("Fries", 2.55, friesImg);
+    const colaItem = createMenuItem("Coca-cola", 1.25, colaImg);
+
+    menuContentDiv.appendChild(menuHeading);
+    menuContentDiv.appendChild(burgerItem);
+    menuContentDiv.appendChild(friesItem);
+    menuContentDiv.appendChild(colaItem);
+
+    return menuContentDiv;
+}
+
+function createMenuItem(name, price, img) {
     const menuItem = document.createElement("div");
     menuItem.classList.add("menu-item");
     
     const itemInfo = document.createElement("div");
     
     const itemName = document.createElement("p");
-    itemName.textContent = "Burger";
+    itemName.textContent = name;
     
     const itemPrice = document.createElement("p");
-    itemPrice.textContent = "Price: 5$";
+    itemPrice.textContent = `Price: ${price}$`;
     
     itemInfo.appendChild(itemName);
     itemInfo.appendChild(itemPrice);
     
     const itemImg = new Image();
     itemImg.classList.add("item-img")
-    itemImg.src = burgerImg;
+    itemImg.src = img;
     
     menuItem.appendChild(itemInfo);
     menuItem.appendChild(itemImg);
 
-    menuContentDiv.appendChild(menuHeading);
-    menuContentDiv.appendChild(menuItem);
-
-    return menuContentDiv;
+    return menuItem;
 }
-
-// function createMenuItem(itemName, itemPrice, itemImg) {
-
-// }
